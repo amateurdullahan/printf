6241b8e5748f96d902c66602ec1e14849d3fb0f4
amateurdullahan <zabrak12@gmail.com>
2020-03-14 18:26:55 +0000

    add strlen func to printf.c

--- printf.c
+++ printf.c
@@ -34,3 +34,18 @@ int _printf(const char *format)
 	}
     }
 }
+
+/**
+ * _strlen - determines length of string
+ * @c: string                                              
+ *
+ * Return: length of string
+*/
+
+int _strlen(char *c) 
+{
+int len;
+for (len = 0; c[len]; len++)
+;
+return (len);
+}
