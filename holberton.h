ce795d399f51da4badb5c7b793b2d13072711db2
Corbin Enterline <1692@holbertonschool.com>
2020-03-14 19:36:47 +0000

    Updated header for new functions

--- holberton.h
+++ holberton.h
@@ -4,4 +4,13 @@
 int _printf(const char *format, ...);
 
 int _strlen(char *c);
+
+void _swap(char *x, char *y);
+
+char *_reverse(char *buffer, int i, int j);
+
+int _abs(int a);
+
+char *_itoa(int value, char *buffer, int base);
+
 #endif /* HOLBERTON_H */
