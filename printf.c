14fe7945724c0482085f7e2e272ac93f3f772fc1
amateurdullahan <zabrak12@gmail.com>
2020-03-14 17:31:25 +0000

    add basic printf.c file

---
+++ printf.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * _printf - basic func to print a string of characters
+ * @format: character string
+ *
+ * Return: number of characters printed
+ */
+
+int _printf(const char *format)
+{
+
+}
