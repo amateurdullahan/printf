8b03594a1454c09ea83e7482e8eaf6c50763eecf
Corbin Enterline <1692@holbertonschool.com>
2020-03-16 01:39:29 +0000

    Adding putchar

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _putchar - writes char to stdout
+ * @c: input char
+ * Return: Char printed to stdout;
+ */
+int _putchar(char c)
+{
+	return (write(1, %c, 1));
+}
