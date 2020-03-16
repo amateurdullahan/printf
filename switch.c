3871cf3871dca6bbe9051c1c1bf0b2a10c7a30bc
amateurdullahan <zabrak12@gmail.com>
2020-03-16 21:38:08 +0000

    fixed being over 40 lines

--- switch.c
+++ switch.c
@@ -11,53 +11,42 @@
 int print_conv(const char *str, va_list arg)
 {
   int count, c;
-  char *s;
-  count = 0;
-  
+  char *s = malloc(sizeof(str));
+  count = 0;  
   str++;
   switch (*str)
     {
     case 's':
       s = va_arg(arg, char *);
       if (s == NULL)
-	{
 	  return (-1);
-	}
       else
       _puts(s);
       count += _strlen(s);
       return (count);
-
     case 'c':
       c = va_arg(arg, int);
       _putchar(c);
       count++;
       return (count);
-
     case 'd':
-      s = malloc(sizeof(str));
       s = _itoa(va_arg(arg, int), s, 10);
       _puts(s);
       count += _strlen(s);
       return (count);
-
     case 'i':
-      s = malloc(sizeof(str));
       s = _itoa(va_arg(arg, int), s, 10);
       _puts(s);
       count += _strlen(s);
       return (count);
-
     case '%':
       str--;
       _putchar('%');
       count++;
       return (count);
-
       default:
       _putchar('%');
       count++;
       return (count);
     }
-  return (count);
-    }
+}
